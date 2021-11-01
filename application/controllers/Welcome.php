cd92066b7e7df80ab8b21617bfa450f52df68036
rizkiamaliya <amaliyarizki86@gmail.com>
2021-11-01 16:41:27 +0700

    Update .editorconfig, .gitignore, and 465 more files...

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		
+		$hasil = $this->Model_Orang->AmbilDataOrang();
+		$data = [
+			"daftar_orang" => $hasil
+			$this->load->view('orang', $data);
+		];
+		
+		
+	}
+	public function TambahOrang() {
+		$this->load->model('Model_Orang');
+		$this->ioad->per('url');
+		$nama = $this->input->post("nama");
+		$nama = $this->input->post("alamat");
+
+		this->Model_Orang ($nama, $alamat);
+
+		redirect ("Welcome");
+	}
+}
